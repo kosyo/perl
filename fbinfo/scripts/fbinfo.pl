b56811f7e2fb12bec79b31e2074056e1e3b29f04
kosyo <kosyokk@gmail.com>
2012-12-20 01:51:59 +0200

    Moved to module FbInfoApp

--- fbinfo/scripts/fbinfo.pl
+++ fbinfo/scripts/fbinfo.pl
@@ -1,23 +1,6 @@
 #!/usr/bin/perl -w
 
-use CGI;
-use strict;
-my $cgi = CGI->new;
-use Net::Facebook::Oauth2;
+use FbInfoApp;
 
-my $app_secret = '4b8b3ced7f75401c4f4d7274546fcc2e';
-my $app_id = '304631152969790';
-my $call_back = 'http://77.85.27.24/perl/fbinfo/scripts/fbinfo_callback.pl';
+FbInfoApp::FacebookAuth();
 
-my $fb = Net::Facebook::Oauth2->new(
-	application_id => $app_id, 
-        application_secret => $app_secret,
-        callback => $call_back
-);
-    
-my $url = $fb->get_authorization_url(
-	scope => ['offline_access','publish_stream'],
-        display => 'page'
-);
-
-print $cgi->redirect($url);
